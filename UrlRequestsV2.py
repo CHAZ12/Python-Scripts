6186cbe7828dcf4245ae2421004fad31156164ac
CHAZ12 <zpmass@gmail.com>
2021-10-09 21:30:16 -0500

    Add files via upload

--- UrlRequestsV2.py
+++ UrlRequestsV2.py
@@ -5,6 +5,7 @@ import re
 import time
 from datetime import datetime
 import json
+import pytz
 
 print('Getting or installing necessary modules')
 #   Inorder for the script to run properly you need all modules below, so make sure they are installed
@@ -53,84 +54,109 @@ except ModuleNotFoundError:
     print('module openpyxl is not installed')
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'openpyxl'])
 
-print("------ THIS IS A SCRIPT TO GET STREAMERS STATISTICS FROM TWITCH.TV -----")
+print("\n------ THIS IS A SCRIPT TO GET STREAMERS STATISTICS FROM TWITCH.TV -----")
 
 
-#   GET READY TO GET STREAMER INFO FROM TWITCH USING REQUESTS_HTML
+def print_stats(followers, twitchChatCount, Game, viewers, date):
 
-def render_func():
-    # Get render Request fro streamer and Content
-    from requests_html import HTMLSession
-    session = HTMLSession()
-    url = ('https://www.twitch.tv/' + streamerName)
-    print('Fetching a valid url render response state for: ' + 'https://www.twitch.tv/' + streamerName)
-    response = session.get(url)
-    if not response.html.render(timeout=10, sleep=10):
-        print('Render response is successful')
-        body = response.html.find('body', first=True).text
-        newTxt = body.split('Expand')[0]
-        # print(newTxt)
-        if re.search(r'(Sign\sUp\nLIVE)', str(newTxt)):
-            print('User is online. Continuing...')
-            regex_func(newTxt, url)
-        else:
-            print('User is offline or does not exists. Moving on')
-            time.sleep(1)
-    else:
-        print('Request Failed, restarting...')
-        time.sleep(1)
-
-
-def regex_func(newTxt, url):
-    #   Up to this point nothing is written yet, so it is safe to close the program.
-    streamer = re.search(r'([^\s\n]*\n(?=Follow))', newTxt).group().strip()
-    print("Streamer: " + streamer)
-    viewers = re.search(r'([^\s]\S{0,10}\n(?=\d+:\d))', newTxt).group().strip()
-    print("Viewers: " + viewers)
-    game = re.search(r'(?<=Subscribe\n)(?:[^\n]*\n)([^\n]+)', newTxt).group(1).strip()
-    print("Game: " + game)
-    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    post_request_func(date, game, viewers, url)
-
-
-def post_request_func(date, game, viewers, url):
-    #  GET TWITCH'S USER IN CHAT DATA DIRECTLY FROM A POST REQUEST
-    print('Fetching Client Id')
-    homepage = requests.get(url).text
-    client_id = re.search('"Client-ID" ?: ?"(.*?)"', homepage).group(1)
-
-    #   Get Client Id and Streamer name
-    headers = {"Client-Id": client_id}  # get Local Client Id
-
-    #    FROM TWITCH REQUESTS GET HOW MANY FOLLOWERS DATA
-    query1 = 'query {user(login: "%s") { followers { totalCount } } }' % streamerName  # Query type
-    data1 = json.dumps([{"query": query1}])
-
-    #    FROM TWITCH REQUESTS GET HOW MANY UserChat DATA
-    query2 = 'query {user(login: "%s")  { channel { chatters  { count } } } }' % streamerName
-    data2 = json.dumps([{"query": query2}])
-
-    #   Post request DATA
-    resp1 = requests.post("https://gql.twitch.tv/gql", data=data1, headers=headers)
-    resp2 = requests.post("https://gql.twitch.tv/gql", data=data2, headers=headers)
-
-    #   make sure I just display the value only
-    followers = resp1.json()[0]["data"]["user"]["followers"]["totalCount"]
-    twitchChatCount = resp2.json()[0]["data"]["user"]["channel"]["chatters"]["count"]  # includes the streamer in count
-
+    print("Streamer: " + str(streamerName))
+    LogFile.write("\nStreamer: " + str(streamerName))
+    print("Viewers: " + str(viewers))
+    LogFile.write("\nViewers: " + str(viewers))
+    print("Game: " + str(Game))
+    LogFile.write("\nGame: " + str(Game))
     print("Followers: " + str(followers))
+    LogFile.write("\nFollowers: " + str(followers))
     print("Users In Chat: " + str(twitchChatCount))
-    create_excel_func(game, followers, viewers, date, twitchChatCount)
+    LogFile.write("\nUsers In Chat: " + str(twitchChatCount))
+    create_excel_func(Game, followers, viewers, date, twitchChatCount)
 
 
-def create_excel_func(game, followers, viewers, date, twitchChatCount):
+def post_request_func():
+
+    #  GET TWITCH'S USER IN CHAT DATA DIRECTLY FROM A POST REQUEST
+    global game, Viewers
+    url = ('https://www.twitch.tv/' + streamerName)
+    timeZ_Ce = pytz.timezone('US/Central')
+    date = datetime.now(timeZ_Ce).strftime('%Y-%m-%d %H:%M:%S')
+    isLive = False
+
+    print('Fetching a valid url response state for: ' + 'https://www.twitch.tv/' + streamerName)
+    LogFile.write(
+        "\nTime:" + date + ', Fetching a valid url response state for: ' + 'https://www.twitch.tv/' + streamerName)
+    homepage = requests.get(url).text
+    if not homepage:
+        print('User page not found. Moving on..')
+        LogFile.write('\nUser page not found. Moving on...')
+    else:
+        print('User page found. Continuing...')
+        LogFile.write('\nUser page found. Continuing...')
+        isLive = True
+
+    if isLive:
+        # Get Client Id just increase it changes
+        print('Fetching Client Id')
+        LogFile.write('\nFetching Client Id')
+        client_id = re.search('"Client-ID" ?: ?"(.*?)"', homepage).group(1)
+        headers = {"Client-Id": client_id}  # get Local Client Id
+
+        # From Twitch Get FOLLOWERS
+        query1 = 'query {user(login: "%s") { followers { totalCount } } }' % streamerName  # Query type
+        data1 = json.dumps([{"query": query1}])
+
+        # From twitch is USERS IN CHAT
+        query2 = 'query {user(login: "%s")  { channel { chatters  { count } } } }' % streamerName
+        data2 = json.dumps([{"query": query2}])
+
+        # From Tiwtch Get is LIVE
+        query3 = 'query {user(login: "%s") { stream { type } } } ' % streamerName
+        data3 = json.dumps([{"query": query3}])
+
+        # From Twitch get GAME name
+        query4 = 'query {user(login: "%s") { stream { game  { name } } } } ' % streamerName
+        data4 = json.dumps([{"query": query4}])
+
+        # From Twitch get VIEWERS
+        query5 = 'query {user(login: "%s") { stream { viewersCount } } } ' % streamerName
+        data5 = json.dumps([{"query": query5}])
+
+        #   Post request DATA
+        resp1 = requests.post("https://gql.twitch.tv/gql", data=data1, headers=headers)
+        resp2 = requests.post("https://gql.twitch.tv/gql", data=data2, headers=headers)
+        resp3 = requests.post("https://gql.twitch.tv/gql", data=data3, headers=headers)
+        resp4 = requests.post("https://gql.twitch.tv/gql", data=data4, headers=headers)
+        resp5 = requests.post("https://gql.twitch.tv/gql", data=data5, headers=headers)
+
+        #   make sure I just output the value I want only
+        followers = resp1.json()[0]["data"]["user"]["followers"]["totalCount"]
+        twitchChatCount = resp2.json()[0]["data"]["user"]["channel"]["chatters"]["count"]
+        live = resp3.json()[0]["data"]["user"]["stream"] or ["type"]
+
+        if not live == ['type']:    # make sure that game is not NONE
+            game = resp4.json()[0]["data"]["user"]["stream"]["game"]["name"]    # Game name
+            Viewers = resp5.json()[0]["data"]["user"]["stream"]["viewersCount"]    # VIEWERS
+
+        if live == ['type']:
+            print('User is offline or does not exists. Moving on...')
+            LogFile.write('\nUser is offline or does not exists. Moving on...')
+        else:
+            print('User is online')
+            LogFile.write('\nUser is offline or does not exists. Moving on...')
+            print_stats(followers, twitchChatCount, game, Viewers, date)
+
+
+def create_excel_func(Game, followers, viewers, date, twitchChatCount):
+
     #   GET READY TO CREATE OR OPEN A EXCEL FILE TO POST NEW INFORMATION
-    File = open('bob', 'a+', encoding='utf-8')
+    print('Creating Excel data')
+    LogFile.write('\nCreating excel data')
+    time.sleep(1)
+    File = open('bob.txt', 'a+', encoding='utf-8')
     File.write(
-        'TwitchName:{0}, Game:{1}, Followers:{2}, Viewers:{3}, Date & Time:{4}, Twitch\'s Users In Chat:{5}'
+        'TwitchName:{0}, Game:{1}, Followers:{2}, Viewers:{3}, Date & Time CDT:{4}, Twitch\'s Users In Chat:{5}'
         '\n\n'.format(
             streamerName,
-            game,
+            Game,
             followers,
             viewers,
             date,
@@ -143,32 +169,41 @@ def create_excel_func(game, followers, viewers, date, twitchChatCount):
     from openpyxl.workbook import Workbook
 
     #   Create or open an Excel file to append gathered values
-    headers = ['TwitchName', 'Game', 'Followers', 'Viewers', 'Date/time', 'Twitch\'s Users In Chat', 'my Users in Chat']
+    headers = ['TwitchName', 'Game', 'Followers', 'Viewers', 'Date & time CDT', 'Twitch\'s Users In Chat']
     wb = Workbook()
     page = wb.active
     page.title = 'Steamer_Statistics'
     page.append(headers)  # write the headers to the first line
 
     #   Data to write:
-    statistics = [streamerName, game, followers, viewers, date, twitchChatCount]
+    statistics = [streamerName, Game, followers, viewers, date, twitchChatCount]
 
     page.append(statistics)
     wb.save(filename=workbook_name)
 
-    #   Append Old to New stuff
+    #   Append Old to New stuff excel files
     old_file = pd.read_excel("OldTiwtch.xlsx")
     New_file = pd.read_excel("NewTwitch.xlsx")
     append_df = pd.concat([old_file, New_file])  # append old and statistics
 
     append_df.to_excel("NewTwitch.xlsx", index=False, sheet_name='Steamer_Statistics')
     print('Excel Complete')
+    LogFile.write('\nExcel Complete')
 
 
-with open("Streamers", 'r') as TxtFile:
-    for line in TxtFile.readlines():
-        streamerName = line.strip('\n')
-        render_func()
+with open("LogFile.txt", 'a+', encoding="utf-8") as LogFile:
+    with open("Streamers.txt", 'r') as TxtFile:
+        for line in TxtFile.readlines():
+            streamerName = line.strip('\n')
+            post_request_func()
+            time.sleep(1)
+
+    LogFile.write('\n------------------Finished-----------')
+    LogFile.close()
+
 print('Finished')
+exit()
+
 # # EXTRAS: MODIFIED EXCEL CODE FROM: https://betterprogramming.pub/,
 #   https://towardsdatascience.com/merging-spreadsheets-with-python-append-f6de2d02e3f3 # GET REQUESTS CODE FROM
 #   https://docs.python-requests.org/en/master/user/quickstart/
